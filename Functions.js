a310015fc5140617e9421d4dcaba02c748b5b5df
JRSumner <joeraysumner@gmail.com>
2023-02-26 15:04:04 +0000

    Create operate function to determine required operation

--- Functions.js
+++ Functions.js
@@ -1,4 +1,22 @@
-const add = (number1, number2) => number1 + number2;
-const subtract = (number1, number2) => number1 - number2;
-const multiply = (number1, number2) => number1 * number2;
-const divide = (number1, number2) => number1 / number2;
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      console.log(
+        'Unable to perform operation, please provide one of the following operators "+, -, *, /"'
+      );
+      break;
+  }
+}
