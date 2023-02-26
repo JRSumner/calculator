7565c618e350a73a7f4c24a3b0a14593400da653
JRSumner <joeraysumner@gmail.com>
2023-02-26 14:57:08 +0000

    Create functions file to house basic math funcs

---
+++ Functions.js
@@ -0,0 +1,4 @@
+const add = (number1, number2) => number1 + number2;
+const subtract = (number1, number2) => number1 - number2;
+const multiply = (number1, number2) => number1 * number2;
+const divide = (number1, number2) => number1 / number2;
