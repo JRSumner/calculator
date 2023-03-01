b4b0fc03709615b96b85a7b0e99493fb97a9f89e
JRSumner <joeraysumner@gmail.com>
2023-03-01 22:01:15 +0000

    Add test case for operate function in math.test.js
    This commit adds a test case to the math.test.js file for the operate()
    function in the math.js module. The test case checks whether the function
    performs arithmetic operations on two numbers with an operator and
    returns the expected result. Additionally, the test checks whether the
    function correctly logs an error message when an invalid operator is
    provided. These tests improve the overall test coverage of the project
    and ensure that the operate() function works correctly.

--- math.test.js
+++ math.test.js
@@ -1,4 +1,4 @@
-import { add, subtract, multiply, divide } from "./math";
+import { add, subtract, multiply, divide, operate } from "./math";
 
 describe("math functions", () => {
   test("add function should add two numbers together", () => {
@@ -23,5 +23,19 @@ describe("math functions", () => {
     expect(divide(6, 3)).toBe(2);
     expect(divide(10, -5)).toBe(-2);
     expect(divide(0, 5)).toBe(0);
+  });
+
+  test("operate function should perform arithmetic operations on two numbers with an operator", () => {
+    expect(operate(2, 3, "+")).toBe(5);
+    expect(operate(10, 5, "-")).toBe(5);
+    expect(operate(4, 5, "*")).toBe(20);
+    expect(operate(10, 2, "/")).toBe(5);
+    const consoleSpy = jest.spyOn(console, "log");
+    expect(operate(2, 3, "%")).toBeUndefined();
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "%",
+      'Unable to perform operation, please provide one of the following operators "+, -, *, /"'
+    );
+    consoleSpy.mockRestore();
   });
 });
